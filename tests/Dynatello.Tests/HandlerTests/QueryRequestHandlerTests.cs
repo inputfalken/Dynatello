737998cf03a6c481b3563d1a60f3a2389182a983
Robert Andersson <inputfalken@gmail.com>
2024-06-06 23:09:19 +0200

    Add chunked test

--- tests/Dynatello.Tests/HandlerTests/QueryRequestHandlerTests.cs
+++ tests/Dynatello.Tests/HandlerTests/QueryRequestHandlerTests.cs
@@ -32,4 +32,37 @@ public class QueryRequestHandlerTests
 
         Assert.Equal(expected, actual);
     }
+
+    [Fact]
+    public async Task Send_SuccessChunkedMock_ShouldReturnAttributes()
+    {
+        var amazonDynamoDB = Substitute.For<IAmazonDynamoDB>();
+        var chunks = Cat.Fixture.CreateMany<Cat>(20).Chunk(5).Select((x, y) => (x, y)).ToArray();
+
+        foreach (var (elements, index) in chunks)
+        {
+            Dictionary<string, AttributeValue> LastEvaluatedKey = index == 0
+              ? new()
+              : LastEvaluatedKey = new() { { nameof(Cat.Id), new AttributeValue() { S = elements[^1].Id.ToString() } } };
+            
+            amazonDynamoDB
+              .QueryAsync(Arg.Is<QueryRequest>(x => x.ExclusiveStartKey == LastEvaluatedKey))
+              .Returns(new QueryResponse
+              {
+                  HttpStatusCode = System.Net.HttpStatusCode.OK,
+                  Items = elements.Select(x => Cat.QueryWithCuteness.Marshall(x)).ToList(),
+                  LastEvaluatedKey = LastEvaluatedKey
+              });
+        }
+
+        var actual = await Cat.QueryWithCuteness
+          .OnTable("TABLE")
+          .ToQueryRequestHandler(x => x
+              .WithKeyConditionExpression(((x, y) => $"{x.Id} = {y.Id}"))
+              .ToQueryRequestBuilder() with
+          { IndexName = "INDEX" }, amazonDynamoDB)
+          .Send((Guid.NewGuid(), 2), default);
+
+        Assert.Equal(chunks.SelectMany(x => x.x), actual);
+    }
 }
