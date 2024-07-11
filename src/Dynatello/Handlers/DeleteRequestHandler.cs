9885ea5904a92598f1379648f56ac35fa726faae
Robert Andersson <inputfalken@gmail.com>
2024-07-11 14:55:53 +0200

    Implement handler

--- src/Dynatello/Handlers/DeleteRequestHandler.cs
+++ src/Dynatello/Handlers/DeleteRequestHandler.cs
@@ -1,10 +1,38 @@
+using Amazon.DynamoDBv2.Model;
+using Dynatello.Pipelines;
+
 namespace Dynatello.Handlers;
 
-internal sealed class DeleteREquestHandler<TArg, T> : IRequestHandler<TArg, T>
+internal sealed class DeleteREquestHandler<TArg, T> : IRequestHandler<TArg, T?>
 {
-    public Task<T> Send(TArg arg, CancellationToken cancellationToken)
+    private readonly HandlerOptions _handlerOptions;
+    private readonly Func<TArg, DeleteItemRequest> _createRequest;
+    private readonly Func<Dictionary<string, AttributeValue>, T> _createItem;
+
+    internal DeleteREquestHandler(
+        HandlerOptions handlerOptions,
+        Func<TArg, DeleteItemRequest> createRequest,
+        Func<Dictionary<string, AttributeValue>, T> createItem
+    )
     {
-        throw new NotImplementedException();
+        _handlerOptions = handlerOptions;
+        _createRequest = createRequest;
+        _createItem = createItem;
+    }
+    public async Task<T?> Send(TArg arg, CancellationToken cancellationToken)
+    {
+        var request = _createRequest(arg);
+        var response = await request
+          .SendRequest(
+            _handlerOptions.RequestsPipelines,
+            (x, y, z) => y.DeleteItemAsync(x, z),
+            _handlerOptions.AmazonDynamoDB,
+            cancellationToken
+          );
+
+        return request.ReturnValues.IsValueProvided()
+          ? _createItem(response.Attributes)
+          : default;
     }
 }
 
